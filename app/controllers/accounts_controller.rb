787989b571282b750952019e35a71bcf0b2c3830
kisana <kisana@amiverse.net>
2026-04-18 18:56:52 +0900

    FIX ダッシュボード accounts 整える

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,59 +1,33 @@
 class AccountsController < ApplicationController
-  before_action :set_account, only: %i[ show edit update destroy ]
   before_action :require_admin
+  before_action :set_account, only: %i[ show update ]
 
   def index
-    @accounts = Account.all
+    accounts = Account.includes(:icon)
+    @accounts = set_pagination_for(accounts)
   end
 
   def show
   end
 
-  def new
-    @account = Account.new
-  end
-
-  def edit
-  end
-
-  def create
-    @account = Account.new(account_params)
-
-    if @account.save
-      redirect_to @account, notice: 'Account was successfully created.'
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
   def update
     if @account.update(account_params)
-      redirect_to @account, notice: 'Account was successfully updated.', status: :see_other
+      redirect_to account_path(@account.aid), notice: '更新しました'
     else
-      render :edit, status: :unprocessable_entity
+      render :show, status: :unprocessable_entity
     end
-  end
-
-  def destroy
-    @account.destroy!
-
-    redirect_to accounts_path, notice: 'Account was successfully destroyed.', status: :see_other
   end
 
   private
 
   def set_account
-    @account = Account.find(params.expect(:id))
+    @account = Account.find_by(aid: params.expect(:aid))
   end
 
   def account_params
     params.expect(
       account: [
-        :name,
-        :name_id,
-        :email,
-        :password,
-        :password_confirmation
+        :status,
       ]
     )
   end
